cd9ec02a432bb759242b6563c30ba0799049938d
jenkinsgithub123 <lysa.dsn@gmail.com>
2016-06-09 05:26:44 +0000

    The 7th

--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -4,7 +4,7 @@ require APPLICATION_PATH . '/Example.php';
 class ExampleTest extends PHPUnit_Framework_TestCase{
 	public function testSomething(){
 		$ex = new Example();
-		$this->assertEquals($ex->foo(),'bar');
+		$this->assertEquals($ex->foo(),'sa');
 	}
 }
 
